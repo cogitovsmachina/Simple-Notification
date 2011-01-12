56b0bb34c8ae2a58300a1614c9f2ed03a904ed48
Enrique Diaz <nrikediaz@gmail.com>
2011-01-12 04:26:40 -0600

    adding vibrate and sound to the notification

--- src/org/androidtitlan/simplenotification/SimpleNotification.java
+++ src/org/androidtitlan/simplenotification/SimpleNotification.java
@@ -6,6 +6,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class SimpleNotification extends Activity {
@@ -22,24 +23,50 @@ public class SimpleNotification extends Activity {
         NotificationManager mNotificationManager = (NotificationManager) getSystemService(ns);
         
         //Instantiate the Notification
+        // icon from resources
         int icon = R.drawable.notification_icon;
-        CharSequence tickerText = "Hello";
+       // ticker-text
+        CharSequence tickerText = "Simple Notification!";
+        // notification time
         long when = System.currentTimeMillis();
         //Creating a Object Notification passing the id of the icon, the flowing text in the notification
         //bar and the time to show in the time field.
         Notification mNotification = new Notification(icon, tickerText, when);
         
+        //Using default Sound
+        //mNotification.defaults = Notification.DEFAULT_SOUND;
+        
+        //Using other sound
+        mNotification.sound = Uri.parse("file:///sdcard/Media/Audio//13 Are You In-.mp3");
+
+        //Configurations of Notification
+        mNotification.flags= Notification.FLAG_AUTO_CANCEL;
+        
+      //Vibrate
+      //  mNotification.defaults= Notification.DEFAULT_VIBRATE;
+        
+        long[] vibrate = {0,500,100,200,0,500,100,200,0};
+        mNotification.vibrate = vibrate;
+        
+        
         //Define the Notification's expanded message and Intent
+        
+        // application Context
         Context context = getApplicationContext();
-        CharSequence contentTitle = "This is the Title";
-        CharSequence contentText = "Hello Notifications!";
+       
+     // expanded message title
+        CharSequence contentTitle = "This is the Title =)";
+        // expanded message text
+        CharSequence contentText = "This is the inner text"; 
         Intent mIntent = new Intent(this, SimpleNotification.class);
       //  Retrieve a PendingIntent that will start a new activity, like calling Context.startActivity(Intent).
         PendingIntent mPendingIntent = PendingIntent.getActivity(this, 0, mIntent, 0);
+       
         
+     // The next two lines initialize the Notification, using the configurations above
         mNotification.setLatestEventInfo(context, contentTitle, contentText, mPendingIntent);
+       
 
-        
        // Pass the Notification to the NotificationManager:
           mNotificationManager.notify(HELLO_ID, mNotification);
           
