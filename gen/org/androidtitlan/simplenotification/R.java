1389c1ff2430aba65f4f5642ad324f28eb2fa5a4
Enrique Diaz <nrikediaz@gmail.com>
2011-01-14 22:02:08 -0600

    adding vibrate and sound to the notification

--- gen/org/androidtitlan/simplenotification/R.java
+++ gen/org/androidtitlan/simplenotification/R.java
@@ -14,8 +14,13 @@ public final class R {
         public static final int icon=0x7f020000;
         public static final int notification_icon=0x7f020001;
     }
+    public static final class id {
+        public static final int image=0x7f050000;
+        public static final int text=0x7f050001;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int custom_notification_layout=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- src/org/androidtitlan/simplenotification/SimpleNotification.java
+++ src/org/androidtitlan/simplenotification/SimpleNotification.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.RemoteViews;
 
 public class SimpleNotification extends Activity {
     public static final int HELLO_ID = 1;
@@ -17,46 +18,90 @@ public class SimpleNotification extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        // createSimpleNotification();  
+        createCustomNotification();
         
-        String ns = Context.NOTIFICATION_SERVICE;
+    }
+
+	private void createCustomNotification() {
+		String ns = Context.NOTIFICATION_SERVICE;
         //Get a reference to the Notification Manager
         NotificationManager mNotificationManager = (NotificationManager) getSystemService(ns);
         
         //Instantiate the Notification
-        // icon from resources
         int icon = R.drawable.notification_icon;
-       // ticker-text
         CharSequence tickerText = "Simple Notification!";
-        // notification time
         long when = System.currentTimeMillis();
         //Creating a Object Notification passing the id of the icon, the flowing text in the notification
         //bar and the time to show in the time field.
         Notification mNotification = new Notification(icon, tickerText, when);
         
-        //Using default Sound
+        //Using Sounds
         //mNotification.defaults = Notification.DEFAULT_SOUND;
         
-        //Using other sound
         mNotification.sound = Uri.parse("file:///sdcard/Media/Audio//13 Are You In-.mp3");
 
         //Configurations of Notification
         mNotification.flags= Notification.FLAG_AUTO_CANCEL;
         
       //Vibrate
-      //  mNotification.defaults= Notification.DEFAULT_VIBRATE;
-        
+      //  mNotification.defaults= Notification.DEFAULT_VIBRATE
         long[] vibrate = {0,500,100,200,0,500,100,200,0};
         mNotification.vibrate = vibrate;
         
         
         //Define the Notification's expanded message and Intent
-        
-        // application Context
         Context context = getApplicationContext();
-       
-     // expanded message title
         CharSequence contentTitle = "This is the Title =)";
-        // expanded message text
+        CharSequence contentText = "This is the inner text"; 
+        Intent mIntent = new Intent(this, SimpleNotification.class);
+      //  Retrieve a PendingIntent that will start a new activity, like calling Context.startActivity(Intent).
+        PendingIntent mPendingIntent = PendingIntent.getActivity(this, 0, mIntent, 0);
+       
+        
+          
+          RemoteViews contentView = new RemoteViews(getPackageName(), R.layout.custom_notification_layout);
+          contentView.setImageViewResource(R.id.image, R.drawable.notification_icon);
+          contentView.setTextViewText(R.id.text, "Hello, this message is in a custom expanded view");
+          mNotification.contentView = contentView;
+
+          Intent notificationIntent = new Intent(this, SimpleNotification.class);
+          PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+          mNotification.contentIntent = contentIntent;
+          mNotificationManager.notify(HELLO_ID , mNotification);
+
+	}
+
+	private void createSimpleNotification() {
+		String ns = Context.NOTIFICATION_SERVICE;
+        //Get a reference to the Notification Manager
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(ns);
+        
+        //Instantiate the Notification
+        int icon = R.drawable.notification_icon;
+        CharSequence tickerText = "Simple Notification!";
+        long when = System.currentTimeMillis();
+        //Creating a Object Notification passing the id of the icon, the flowing text in the notification
+        //bar and the time to show in the time field.
+        Notification mNotification = new Notification(icon, tickerText, when);
+        
+        //Using Sounds
+        //mNotification.defaults = Notification.DEFAULT_SOUND;
+        
+        mNotification.sound = Uri.parse("file:///sdcard/Media/Audio//13 Are You In-.mp3");
+
+        //Configurations of Notification
+        mNotification.flags= Notification.FLAG_AUTO_CANCEL;
+        
+      //Vibrate
+      //  mNotification.defaults= Notification.DEFAULT_VIBRATE
+        long[] vibrate = {0,500,100,200,0,500,100,200,0};
+        mNotification.vibrate = vibrate;
+        
+        
+        //Define the Notification's expanded message and Intent
+        Context context = getApplicationContext();
+        CharSequence contentTitle = "This is the Title =)";
         CharSequence contentText = "This is the inner text"; 
         Intent mIntent = new Intent(this, SimpleNotification.class);
       //  Retrieve a PendingIntent that will start a new activity, like calling Context.startActivity(Intent).
@@ -65,11 +110,6 @@ public class SimpleNotification extends Activity {
         
      // The next two lines initialize the Notification, using the configurations above
         mNotification.setLatestEventInfo(context, contentTitle, contentText, mPendingIntent);
-       
-
-       // Pass the Notification to the NotificationManager:
-          mNotificationManager.notify(HELLO_ID, mNotification);
-          
-        
-    }
+          mNotificationManager.notify(HELLO_ID, mNotification);		
+	}
 }
